1c796cf6d02907ec72038ebf75856e265d4d3c97
Tuomo Tammenpaa <tuomo.tammenpaa@gmail.com>
2018-02-01 20:19:29 +0000

    stockplay 2chn

--- stocksound.py
+++ stocksound.py
@@ -7,10 +7,21 @@ import Adafruit_MCP4725
 auth = "SNbqZypqf-CzSt51ZQkL"
 dac1 = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1)
 dac2 = Adafruit_MCP4725.MCP4725(address=0x61, busnum=1)
-df = quandl.get("WIKI/AAPL", authtoken=auth)
+df1 = quandl.get("WIKI/AAPL", authtoken=auth)
+df2 = quandl.get("WIKI/MSFT", authtoken=auth)
 
-close_price_df = df["Close"]
-dac_data = close_price_df.as_matrix()
+stock1 = df1["Close"]
+stock2 = df2["Close"]
 
-for x in range(0, dac_data.size):
-    dac1.set_voltage(dac_data[x])
+dac1_data = stock1.as_matrix()
+dac2_data = stock2.as_matrix()
+
+def playStock():
+	for x in range(0, dac1_data.size):
+		dac1.set_voltage(int(dac1_data[x]))
+		dac2.set_voltage(int(dac2_data[x]))
+		print (dac1_data[x], dac2_data[x])
+		#time.sleep(1)
+
+while True:
+	playStock()

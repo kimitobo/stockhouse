0d4b0fe3cbd47192cbca2592de150325aede7fd5
Tuomo <tuomo.tammenpaa.gmail.com>
2018-02-02 00:55:56 +0200

    test scalar

--- stocksound.py
+++ stocksound.py
@@ -2,7 +2,7 @@
 import pandas
 import quandl
 import time
-import Adafruit_MCP4725 
+import Adafruit_MCP4725
 
 auth = "SNbqZypqf-CzSt51ZQkL"
 dac1 = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1)
@@ -13,13 +13,16 @@ df2 = quandl.get("WIKI/MSFT", authtoken=auth)
 stock1 = df1["Close"]
 stock2 = df2["Close"]
 
+scalar1 = 4096 / stock1.max()
+scalar2 = 4096 / stock2.max()
+
 dac1_data = stock1.as_matrix()
 dac2_data = stock2.as_matrix()
 
 def playStock():
 	for x in range(0, dac1_data.size):
-		dac1.set_voltage(int(dac1_data[x]))
-		dac2.set_voltage(int(dac2_data[x]))
+		dac1.set_voltage(int(dac1_data[x]*scalar1))
+		dac2.set_voltage(int(dac2_data[x]*scalar2))
 		print (dac1_data[x], dac2_data[x])
 		#time.sleep(1)
 

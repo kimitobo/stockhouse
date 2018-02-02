a7924ad9173f76a1d320cd680abec92c6d3a4542
Tuomo Tammenpaa <tuomo.tammenpaa@gmail.com>
2018-02-02 00:14:12 +0000

    timing

--- stocksound.py
+++ stocksound.py
@@ -8,7 +8,7 @@ auth = "SNbqZypqf-CzSt51ZQkL"
 dac1 = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1)
 dac2 = Adafruit_MCP4725.MCP4725(address=0x61, busnum=1)
 df1 = quandl.get("WIKI/AAPL", authtoken=auth)
-df2 = quandl.get("WIKI/MSFT", authtoken=auth)
+df2 = quandl.get("WIKI/AAPL", authtoken=auth)
 
 stock1 = df1["Close"]
 stock2 = df2["Close"]
@@ -19,12 +19,17 @@ scalar2 = 4096 / stock2.max()
 dac1_data = stock1.as_matrix()
 dac2_data = stock2.as_matrix()
 
+if dac1_data.size >= dac2_data.size:
+	data_range = dac2_data.size
+else:
+	data_range = dac1_data.size
+
 def playStock():
-	for x in range(0, dac1_data.size):
+	for x in range(0, data_range):
 		dac1.set_voltage(int(dac1_data[x]*scalar1))
 		dac2.set_voltage(int(dac2_data[x]*scalar2))
 		print (dac1_data[x], dac2_data[x])
-		#time.sleep(1)
+		time.sleep(0.1)
 
 while True:
 	playStock()
